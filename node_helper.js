b9a44fe61b3cfe9ade58ded19e7faebfb354150c
Chris Koutavas <christopher.koutavas@gmail.com>
2020-05-17 18:53:34 -0500

    Create node_helper.js

---
+++ node_helper.js
@@ -0,0 +1,20 @@
+/// node_helper.js
+const spawn = require("child_process").spawn
+var NodeHelper = require("node_helper")
+
+module.exports = NodeHelper.create({
+  socketNotificationReceived: function(notification, payload) {
+    switch(notification) {
+      case "GIVE_ME_DATA":
+        this.job()
+        break
+    }
+  },
+  job: function() {
+    var process = spawn("python3", ["/home/pi/MagicMirror/modules/PiTemp/temp.py"])
+    process.stdout.on("data", (data)=>{
+      console.log(data)
+      this.sendSocketNotification("HERE_IS_DATA", data.toString())
+    })
+  }
+})
