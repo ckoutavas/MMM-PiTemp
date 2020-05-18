4d4544a678e4a3f4e1b77c435b1a746f570d162a
Chris Koutavas <christopher.koutavas@gmail.com>
2020-05-18 16:50:20 -0500

    Update MMM-PiTemp.js

--- MMM-PiTemp.js
+++ MMM-PiTemp.js
@@ -1,59 +1,44 @@
 Module.register("PiTemp", {
+	defaults: {
+    	freq: 60000,
+    	high: 80,
+    	low: 70,
+    	highColor: "red",
+    	lowColor: "green",
+    	otherColor: "yellow"
+	},
 	
-  defaults: {
-    freq: 60000,
-    high: 80,
-    low: 70,
-    highColor: "red",
-    lowColor: "green",
-    otherColor: "yellow"
-  },
+	start: function() {
+   		this.sendSocketNotification("GIVE_ME_DATA");
+  	},
 
-  getDom: function() {
+  	getDom: function() {
+   		 var e = document.createElement("div")
+   		 e.id = "DISPLAY"
+		return e
+	},
 
-    var e = document.createElement("div")
+ 	 notificationReceived: function(notification, payload, sender) {
+	 	switch(notification) {
+      			case "DOM_OBJECTS_CREATED":
+        		var timer = setInterval(()=>{
+				this.sendSocketNotification("GIVE_ME_DATA")
+        		}, this.config.freq)
+        		break
+    		}
+	},
 
-    e.id = "DISPLAY"
+  	socketNotificationReceived: function(notification, payload) {
+		switch(notification) {
+      			case "HERE_IS_DATA":
+        		var e = document.getElementById("DISPLAY")
+			if (parseFloat(payload) <= this.config.low) {e.style.color = this.config.lowColor;}
+			else if (parseFloat(payload) >= this.config.high) {e.style.color = this.config.HighColor}
+			else {e.style.color = this.config.otherColor}
+			e.innerHTML = "CPU: " + payload.toString() + "°C";
+       			 break;
+		}
 
-    return e
-
-  },
-
-  notificationReceived: function(notification, payload, sender) {
-
-    switch(notification) {
-
-      case "DOM_OBJECTS_CREATED":
-
-        var timer = setInterval(()=>{
-
-          this.sendSocketNotification("GIVE_ME_DATA")
-
-        }, this.config.freq)
-
-        break
-
-    }
-
-  },
-
-  socketNotificationReceived: function(notification, payload) {
-
-
-    switch(notification) {
-
-      case "HERE_IS_DATA":
-
-        var e = document.getElementById("DISPLAY")
-	if (parseFloat(payload) <= this.config.low) {e.style.color = this.config.lowColor;}
-	else if (parseFloat(payload) >= this.config.high) {e.style.color = this.config.HighColor}
-	else {e.style.color = this.config.otherColor}
-
-        e.innerHTML = "CPU: " + payload.toString() + "°C";
-        break;
-
-    }
-
-  },
+  	},
 
 })
