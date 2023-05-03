d486624ef8a25c49e589a32df9b19f37b75dadf9
Chris Koutavas <christopher.koutavas@gmail.com>
2023-05-03 09:13:13 -0500

    use cwd instead of hard-coded path

--- node_helper.js
+++ node_helper.js
@@ -11,7 +11,7 @@ module.exports = NodeHelper.create({
   },
   
   job: function() {
-    var process = spawn("python3", ["/home/pi/MagicMirror/modules/MMM-PiTemp/temp.py"])
+    var process = spawn("python3", [__dirname + "/temp.py"])
     process.stdout.on("data", (data)=>{
       console.log(data)
       this.sendSocketNotification("temperature", data.toString())
